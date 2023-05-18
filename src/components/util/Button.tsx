63dde968f7b8a16588dba1f297e41ce7281a6f33
Nick Valdez <nicholastvaldez@gmail.com>
2023-05-17 21:05:35 -0500

    remove inline style

--- src/components/util/Button.tsx
+++ src/components/util/Button.tsx
@@ -8,15 +8,7 @@ interface Props {
 const Button = ({ children, link }: Props) => {
   return (
     <Link className="nav-link" to={link}>
-      <button
-        className="main-button font-primary text-white font-bold bg-green rounded duration-500 hover:bg-[#1cd25c]"
-        style={{
-          marginLeft: "67px",
-          marginTop: "10px",
-          height: "35px",
-          width: "103px",
-        }}
-      >
+      <button className="main-button font-primary text-white font-bold bg-green rounded duration-500 hover:bg-[#1cd25c] ml-[67px] mt-[10px] h-[35px] w-[103px]">
         {children}
       </button>
     </Link>
