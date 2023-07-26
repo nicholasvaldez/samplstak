4158d434a721cf297b5fef9a9d1467469b7afd41
Nick Valdez <nicholastvaldez@gmail.com>
2023-07-26 15:47:58 -0500

    added typescript to logic of file input

--- src/components/mysounds/MySoundsForm.tsx
+++ src/components/mysounds/MySoundsForm.tsx
@@ -1,10 +1,58 @@
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 import { NavBar } from "../nav/NavBar"
 import { useNavigate, useParams } from "react-router-dom"
 
-function MySoundsForm() {
+interface Props {
+  token: string
+}
+
+function MySoundsForm({ token }: Props) {
+  const [file, setFile] = useState<File | undefined>(undefined)
+
   const navigate = useNavigate()
   const { sampleId } = useParams()
+
+  const [currentSample, setCurrentSample] = useState({
+    id: 0,
+    producer: parseInt(token),
+    file_url: "",
+    file_name: "",
+    instrument: 0,
+    genre: [],
+    drumkit: null,
+  })
+
+  const getBase64 = (
+    file: File,
+    callback: (base64AudioString: string) => void
+  ) => {
+    const reader = new FileReader()
+    reader.addEventListener("load", () => callback(reader.result as string))
+    reader.readAsDataURL(file)
+  }
+
+  const createAudioString = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    if (!files || files.length === 0) {
+      return
+    }
+
+    getBase64(files[0], (base64AudioString) => {
+      console.log("Base64 of file is", base64AudioString)
+      const copy = { ...currentSample }
+      copy.file_url = base64AudioString
+      setCurrentSample(copy)
+    })
+  }
+
+  const wrapperFunc = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    if (!files || files.length === 0) {
+      return
+    }
+    createAudioString(e)
+    setFile(files[0])
+  }
 
   return (
     <>
@@ -20,7 +68,17 @@ function MySoundsForm() {
               Add a sample.
             </div>
           )}
-          <section className="flex justify-center flex-col items-center"></section>
+          <div className="flex justify-center flex-col items-center">
+            <div className="form-group font-primary ">
+              <input
+                type="file"
+                multiple
+                id="sample_audio"
+                onChange={wrapperFunc}
+              />
+              <input type="hidden" name="sample_id" value={currentSample.id} />
+            </div>
+          </div>
         </main>
       </>
     </>
